bc5fe8b0a847250b2371fd0590967dc78cef8508
Simbok_pc <Simbok_pc@DESKTOP-P39KVI1>
2018-07-25 11:08:07 +0700

    Pada pemangkatan dikurangi fungsi for dan menambah fungsi i Pada faktorial, permutasi, kombinasi menambah kondisi untuk minus

--- src/tools/Nomor1.java
+++ src/tools/Nomor1.java
@@ -13,8 +13,8 @@ public class Nomor1 {
 
     /**
      * fungsi ini digunakan untuk menghitung perkalian
-     * @param a integer
-     * @param b integer
+     * @param a integer menentukan panjang karatkter yang akan diinput 
+     * @param b integer menentukan panjang karatkter yang akan diinput
      * @return kali;
      */
     public int perkalian(int a, int b) {
@@ -25,65 +25,77 @@ public class Nomor1 {
 
     /**
      * Fungsi ini digunakan untuk menghitung bilangan berpangkat
-     * @param nilai integer
-     * @param pangkat integer
+     * @param nilai integer untuk panjang karakter
+     * @param pangkat integer untuk panjang karakter
      * @return hasil;
      */
     public double pemangkatan(int nilai, int pangkat) {
         int i;
         double hasil = 1;
+        double param = pangkat;
         if (pangkat < 0) {
-            pangkat = pangkat * -1;
-
-            for (i = pangkat; i > 0; i--) {
-                hasil = perkalian((int) hasil, nilai);
-            }
-
-            hasil = 1 / hasil;
-
-        } else {
-            for (i = 0; i < pangkat; i++) {
-                hasil = perkalian((int) hasil, nilai);
-            }
+            param = pangkat * -1; 
         }
+        
+        for (i = 0; i < param; i++) { 
+            hasil = perkalian((int) hasil, nilai);
+        }
+
+        if (pangkat < 0) {
+            hasil = 1 / hasil;
+        }
+
         return hasil;
     }
 
     /**
      * fungsi ini digunakan untuk menghitung bilangan faktorial
-     * @param angka integer
-     * @return (angka * faktorial(angka - 1));
+     * @param angka integer untuk panjang karakter
+     * @return (angka * faktorial(angka - 1)); //merupakan fungsi rekursif = memanggil dirinya sendiri
      */
-    public int faktorial(int angka) {
+    public int faktorial(int angka) { 
         if (angka == 0) {
             return 1;
+        } else if (angka > 0) {
+            return (angka * faktorial(angka - 1)); 
         } else {
-            return (angka * faktorial(angka - 1));
+            return 0;
         }
     }
 
     /**
      * fungsi ini digunakan untuk menghitung permutasi
-     * @param n integer
-     * @param r integer
+     * @param n integer untuk panjang karakter. n untuk mengisikan angka pertama
+     * @param r integer untuk panjang karakter. r untuk mengisikan angka kedua
      * @return permutasi;
      */
     public int permutasi(int n, int r) {
         int permutasi;
-        permutasi = faktorial(n) / faktorial(n - r);
+
+        if (n < 0) {
+            permutasi = 0;
+        } else {
+            permutasi = faktorial(n) / faktorial(n - r);
+        }
 
         return permutasi;
+
     }
 
     /**
      * fungsi ini digunakan untuk menghitung kombinasi
-     * @param k integer
-     * @param l integer
+     * @param k integer untuk panjang karakter. k untuk mengisikan angka pertama
+     * @param l integer untuk panjang karakter. l untuk mengisikan angka kedua
      * @return kombinasi;
      */
     public int kombinasi(int k, int l) {
         int kombinasi;
-        kombinasi = faktorial(k) / (faktorial(l) * (faktorial(k - l)));
+
+        if (k < 0) {
+            kombinasi = 0;
+        } else {
+            kombinasi = faktorial(k) / (faktorial(l) * (faktorial(k - l)));
+        }
 
         return kombinasi;
     }
